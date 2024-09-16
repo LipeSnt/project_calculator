2710efe470ccb053d4edfca03321c08a5b704464
Felipe Santos <Felipe_Snt@proton.me>
2024-09-16 16:45:20 -0300

    first commit

---
+++ app/page.js
@@ -0,0 +1,66 @@
+'use client'
+import { useState } from "react";
+
+export default function Home() {
+  const [result, setResult] = useState('');
+  const [expression, setExpression] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        const evalResult = eval(expression).toString();
+        setResult(evalResult);
+        setExpression(evalResult);
+      } catch (error) {
+        setResult('error');
+      } 
+    } else if (value === 'c') {
+      setResult('');
+      setExpression('');
+    } else {
+      setExpression((prevExpression) => prevExpression + value);
+    }
+  };
+
+  const buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '=', '+',
+    'c'
+  ];
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className='text-4xl font-bold mb-10'>Calculator</h1>
+      
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        <input
+          type="text"
+          className="w-full mb-2 text-3xl border-b-2 border-gray-400 focus:outline-none"
+          value={expression}
+          readOnly
+        />
+
+        <input
+          type="text"
+          className="w-full text-4xl font-bold mb-4 focus-outline-none"
+          value={result}
+          readOnly
+        />
+
+        <div className="grid grid-cols-4 gap-2">
+          {buttons.map((btn) => ( 
+            <button
+              key={btn}
+              onClick={() => handleButtonClick(btn)}
+              className="text-4xl bg-gray-300 hover:bg-gray-400 p-2 rounded-lg"
+            >
+              {btn}
+            </button>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
